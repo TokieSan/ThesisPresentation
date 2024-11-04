c14a488700047fe288382960c38f95c50265d1bb
Raef-Hany <98807473+Raef-Hany@users.noreply.github.com>
2024-11-04 14:33:12 +0200

    fine tuning added 2

--- components/sections.js
+++ components/sections.js
@@ -185,7 +185,7 @@
         "details": [
           {
             "type": "image",
-            "content": "https://svgshare.com/s/1CD2"
+            "content": "https://svgshare.com/s/1CD2https://svgshare.com/i/1CD2.svg"
           },
           {
             "type": "text",
